90a1002c93912f6642019f9fa9583df1ead489a6
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-01 14:33:42 -0700

    Messing around with simple classes and now.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,25 @@
 // I know about how to "using namespace std;"
 
 
-class MyPerson
+class Person
 {
+private:
+    std::string name;
+    int age;
+    int weight;
 
+public:
+    void getName();
+    void getAge();
+    void getWeight();
+    Person(std::string name, int age, int weight);
+
+protected:
+    void setAge();
+    void setName();
+    void setWeight();
+    
+    
 };
 
 
@@ -17,12 +33,14 @@ int main()
     
     char cornyMsg[] = "Hello World!";
     const std::string  newString = "Another way of handling strings!";
-
+    int newStrLength;
+    newStrLength = newString.length();
+    
     std::cout << "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*" << std::endl;
     std::cout << "*                   Greetings:                   *" << std::endl;
     std::cout << "*                  "<<cornyMsg<<"                  *" << std::endl;
     std::cout << "*        "<<newString<<"        *" << std::endl;
-    std::cout << "*                                                *" << std::endl;
+    std::cout << "*      The new String was "<< newStrLength <<" Characters long     *" << std::endl;
     std::cout << "*                                                *" << std::endl;
     std::cout << "*                                                *" << std::endl;
     std::cout << "*                                                *" << std::endl;
