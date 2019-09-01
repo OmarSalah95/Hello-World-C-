8fbde2a06aa3486db004df7319dcfe7f9ecdff1e
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-01 14:08:11 -0700

    Messing with basic data types

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
+#include <string>
+// I know about how to "using namespace std;"
+
+
+class MyPerson
+{
+
+};
+
+
+
+
 int main()
 {
+    
+    char cornyMsg[] = "Hello World!";
+    const std::string  newString = "Another way of handling strings!";
+
     std::cout << "*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*" << std::endl;
-    std::cout << "*                                                *" << std::endl;
-    std::cout << "*                                                *" << std::endl;
+    std::cout << "*                   Greetings:                   *" << std::endl;
+    std::cout << "*                  "<<cornyMsg<<"                  *" << std::endl;
+    std::cout << "*        "<<newString<<"        *" << std::endl;
     std::cout << "*                                                *" << std::endl;
     std::cout << "*                                                *" << std::endl;
     std::cout << "*                                                *" << std::endl;
